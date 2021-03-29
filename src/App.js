a2a39903d686b627912888fd38687b71c8c0a301
Ketan Shukla <ketankshukla@gmail.com>
2021-03-29 00:18:17 -0700

    booklist using an array of objects

--- src/App.js
+++ src/App.js
@@ -5,19 +5,10 @@ import BookList from './BookList';
 function App() {
   return (
     <>
-      {/* <Person/>
-      <Message /> */}
       <BookList/>
     </>
   );
 }
 
-//implicit return when only one line needs to be returned
-const Person = () => <h3>John Doe</h3>
-
-//explicit return
-const Message = () => {
-  return <p>this is my message</p>
-}
 
 export default App;
--- src/Book.js
+++ src/Book.js
@@ -1,11 +1,10 @@
-const Book = ({image, title, author, children}) => {
-
+const Book = (props) => {
+const { image, title, author } = props.book
   return (
     <article className='book'>
       <img src={image} alt='' />
       <h4>{title}</h4>
       <h4>{author}</h4>
-      {children}
     </article>
   )
 }
