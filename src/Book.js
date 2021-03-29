b2eb2a73eabafe9a171c4fa09664a77e42c2ef6b
Ketan Shukla <ketankshukla@gmail.com>
2021-03-29 00:48:34 -0700

    added events

--- src/Book.js
+++ src/Book.js
@@ -1,9 +1,25 @@
+import { computeHeadingLevel } from "@testing-library/dom"
+
 const Book = ({ image, title, author }) => {
+  const clickHandler = () => {
+    alert('Hello world!')
+  }
+  const complexExample = (author) => {
+    console.log(author)
+  }
   return (
-    <article className='book'>
+    <article className='book' onMouseOver={() => {
+      console.log(title)
+    }}>
       <img src={image} alt='' />
-      <h4>{title}</h4>
+      <h1>{title}</h1>
       <h4>{author}</h4>
+      <button type='button' onClick={clickHandler}>
+        Reference Example
+      </button>
+      <button type='button' onClick={() => complexExample(author)}>
+        Complex Example
+      </button>
     </article>
   )
 }
