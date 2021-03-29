5daedc662d8a04a0ee7f0f03424f77f5362f8d7a
Ketan Shukla <ketankshukla@gmail.com>
2021-03-29 00:32:14 -0700

    removed books object from the BookList component

--- src/BookList.js
+++ src/BookList.js
@@ -1,30 +1,6 @@
 import Book from './Book'
 import bookdata from './BookData'
 
-const books = [
-  {
-    id: 1,
-    image:
-      'https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg',
-    title: 'I love you to the moon and back',
-    author: 'Amelia Hepworth'
-  },
-  {
-    id: 2,
-    image:
-      'https://images-na.ssl-images-amazon.com/images/I/81nzxODnaJL._AC_UL200_SR200,200_.jpg',
-    title: 'If animals kissed goodnight',
-    author: 'Ann Whitford Paul'
-  },
-  {
-    id: 3,
-    image:
-      'https://images-na.ssl-images-amazon.com/images/I/81lCGooWS0L._AC_UL200_SR200,200_.jpg',
-    title: 'Book of bunny farts',
-    author: 'Humour Heals Us'
-  }
-]
-
 const BookList = () => {
   return (
     <section className='booklist'>
